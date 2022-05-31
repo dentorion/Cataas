cdfd7f030317655b9d990a8a3ddd3f313ca56f71
Denys <dentor2005@gmail.com>
2022-05-31 09:54:56 +0200

    App, MainActivity, MainScreenFragment add Hilt annotations. Internet Permission Manifest.

--- app/src/main/java/com/entin/cataas/presentation/screens/main/MainScreenFragment.kt
+++ app/src/main/java/com/entin/cataas/presentation/screens/main/MainScreenFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.entin.cataas.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainScreenFragment : Fragment() {
 
     override fun onCreateView(

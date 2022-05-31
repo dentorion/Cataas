3367db1c42b09ec6336035ef68aded8489a360e3
Denys <dentor2005@gmail.com>
2022-05-31 08:28:39 +0200

    Catalogue structure in all modules.

--- app/src/main/java/com/entin/cataas/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/entin/cataas/presentation/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.entin.cataas
+package com.entin.cataas.presentation.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.entin.cataas.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

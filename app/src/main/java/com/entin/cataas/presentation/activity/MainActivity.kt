c3d87cfe02d9cd408abe63fe6cde0912b193dad2
Denys <dentor2005@gmail.com>
2022-05-31 09:36:31 +0200

    Application Icon + Splash Screen for light & dark themes.

--- app/src/main/java/com/entin/cataas/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/entin/cataas/presentation/activity/MainActivity.kt
@@ -2,11 +2,14 @@ package com.entin.cataas.presentation.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.entin.cataas.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+
         setContentView(R.layout.activity_main)
     }
 }

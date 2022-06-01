f4c06d734170107f14602b2e97737035d0ee3d68
Denys <dentor2005@gmail.com>
2022-06-01 18:03:11 +0200

    LiveDataExtension.

---
+++ app/src/main/java/com/entin/cataas/presentation/utils/LiveDataExtension.kt
@@ -0,0 +1,13 @@
+package com.entin.cataas.presentation.utils
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+
+/**
+ * Wrapper under LiveData Observer
+ */
+
+fun <T> LifecycleOwner.observe(liveData: LiveData<T>, observer: Observer<T>) {
+    liveData.observe(this, observer)
+}
